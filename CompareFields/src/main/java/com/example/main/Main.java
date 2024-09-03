c8b756d251f33945b6429773ea9cd8677ca861e6
Ashok Vasari/ SID Global <ashokv@sidgs.com>
2024-09-03 18:06:01 +0530

    compare fields

---
+++ CompareFields/src/main/java/com/example/main/Main.java
@@ -0,0 +1,65 @@
+package com.example.main;
+
+import com.example.model.Icore;
+import com.example.model.Npci;
+import com.example.model.Transaction;
+
+import java.io.InputStream;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Icore> icores = new ArrayList<>();
+
+        icores = IntStream.rangeClosed(0, 8000000)
+                 .parallel()
+                .mapToObj(i -> new Icore("adasd","sdsd","gtrtgrt","adasd","sdsd","gtrtgrt","adasd","sdsd","gtrtgrt","adasd","sdsd"))
+                .collect(Collectors.toList());
+
+        icores.add(new Icore("adasd","sdsd","25-12-1986","123","sdsd","gtrtgrt","adasd","sdsd","gtrtgrt","200000","sdsd"));
+
+        List<Npci> npci = new ArrayList<>();
+
+        Random r = new Random();
+        npci = IntStream.rangeClosed(1, 8000000)
+                .parallel()
+                .mapToObj(i -> new Npci("sdas"+r.nextInt(),"sdsd","gtrtgrt","adasd","sdsd","dsfsdfsdf"+r.nextInt(),"200000","sdsd","adasd","sdsd","25-12-1986","adasd","sdsd","gtrtgrt","adasd","sdsd","gtrtgrt","200000","sdsd","sdsd","gtrtgrt","adasd","sdsd","gtrtgrt","200000","sdsd","sdasd","dscsdsa","sdasd","dscsdsa"))
+                .collect(Collectors.toList());
+
+        npci.add(new Npci("123","sdsd","gtrtgrt","adasd","sdsd","25-12-1986","200000","sdsd","adasd","sdsd","25-12-1986","adasd","sdsd","gtrtgrt","adasd","sdsd","gtrtgrt","200000","sdsd","sdsd","gtrtgrt","adasd","sdsd","gtrtgrt","200000","sdsd","sdasd","dscsdsa","sdasd","dscsdsa"));
+
+        long startTime = System.nanoTime();
+
+        Set<Transaction> transactions = icores.stream()
+                .map(p -> new Transaction(p.getTranId(), p.getTranDate()))
+                .collect(Collectors.toSet());
+
+        // Find common fields in employeeList
+        List<Npci> commonRecords = npci.stream()
+                .filter(e -> transactions.contains(new Transaction(e.getTxId(), e.getTranDate())))
+                .toList();
+                //.collect(Collectors.toList());
+
+
+        System.out.println("Common records between 2 lists : "+ commonRecords);
+
+        // Now, 'persons' contains 2 million Person objects
+        System.out.println("Size of the list: " + icores.size()+" "+npci.size());
+        // Optionally, print the first 10 records to verify
+        //persons.stream().limit(10).forEach(System.out::println);
+
+
+        long endTime = System.nanoTime();
+
+        // Calculate the duration in milliseconds
+        long duration = (endTime - startTime) / 1000000;
+
+        System.out.println("Time taken to add 2 million records: " + duration + " ms");
+
+
+     //   System.out.println("Hello world!");
+    }
+}
---
+++ CompareFields/src/main/java/com/example/model/Npci.java
@@ -0,0 +1,362 @@
+package com.example.model;
+
+import java.util.Objects;
+
+public class Npci {
+
+    private String txId;
+    private String transactionType;
+    private String col2Filler;
+    private String tranSrlNo;
+    private String responseCode;
+    private String tranDate;
+    private String tranTime;
+    private String tranAmount;
+    private String col3Filler;
+    private String col4Filler;
+    private String purposeCode;
+    private String col7Filler;
+    private String col8Filler;
+    private String payerVpa;
+    private String col9Filler;
+    private String col10Filler;
+    private String payeeVpa;
+    private String participantId;
+    private String remIfscCode;
+    private String col11Filler;
+    private String remAccountNo;
+    private String col12Filler;
+    private String col13Filler;
+    private String col14Filler;
+    private String col15Filler;
+    private String institutionCode;
+    private String countryCode;
+    private String currencyCode;
+    private String tranAmtIntCur;
+    private String markupRebate;
+    public Npci() {
+    }
+
+
+    public Npci(String txId, String transactionType, String col2Filler, String tranSrlNo, String responseCode, String tranDate, String tranTime, String tranAmount, String col3Filler, String col4Filler, String purposeCode, String col7Filler, String col8Filler, String payerVpa, String col9Filler, String col10Filler, String payeeVpa, String participantId, String remIfscCode, String col11Filler, String remAccountNo, String col12Filler, String col13Filler, String col14Filler, String col15Filler, String institutionCode, String countryCode, String currencyCode, String tranAmtIntCur, String markupRebate) {
+        this.txId = txId;
+        this.transactionType = transactionType;
+        this.col2Filler = col2Filler;
+        this.tranSrlNo = tranSrlNo;
+        this.responseCode = responseCode;
+        this.tranDate = tranDate;
+        this.tranTime = tranTime;
+        this.tranAmount = tranAmount;
+        this.col3Filler = col3Filler;
+        this.col4Filler = col4Filler;
+        this.purposeCode = purposeCode;
+        this.col7Filler = col7Filler;
+        this.col8Filler = col8Filler;
+        this.payerVpa = payerVpa;
+        this.col9Filler = col9Filler;
+        this.col10Filler = col10Filler;
+        this.payeeVpa = payeeVpa;
+        this.participantId = participantId;
+        this.remIfscCode = remIfscCode;
+        this.col11Filler = col11Filler;
+        this.remAccountNo = remAccountNo;
+        this.col12Filler = col12Filler;
+        this.col13Filler = col13Filler;
+        this.col14Filler = col14Filler;
+        this.col15Filler = col15Filler;
+        this.institutionCode = institutionCode;
+        this.countryCode = countryCode;
+        this.currencyCode = currencyCode;
+        this.tranAmtIntCur = tranAmtIntCur;
+        this.markupRebate = markupRebate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Npci npci = (Npci) o;
+        return Objects.equals(getTxId(), npci.getTxId()) && Objects.equals(getTranDate(), npci.getTranDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTxId(), getTranDate());
+    }
+
+    public String getTxId() {
+        return txId;
+    }
+
+    public void setTxId(String txId) {
+        this.txId = txId;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getCol2Filler() {
+        return col2Filler;
+    }
+
+    public void setCol2Filler(String col2Filler) {
+        this.col2Filler = col2Filler;
+    }
+
+    public String getTranSrlNo() {
+        return tranSrlNo;
+    }
+
+    public void setTranSrlNo(String tranSrlNo) {
+        this.tranSrlNo = tranSrlNo;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getTranDate() {
+        return tranDate;
+    }
+
+    public void setTranDate(String tranDate) {
+        this.tranDate = tranDate;
+    }
+
+    public String getTranTime() {
+        return tranTime;
+    }
+
+    public void setTranTime(String tranTime) {
+        this.tranTime = tranTime;
+    }
+
+    public String getTranAmount() {
+        return tranAmount;
+    }
+
+    public void setTranAmount(String tranAmount) {
+        this.tranAmount = tranAmount;
+    }
+
+    public String getCol3Filler() {
+        return col3Filler;
+    }
+
+    public void setCol3Filler(String col3Filler) {
+        this.col3Filler = col3Filler;
+    }
+
+    public String getCol4Filler() {
+        return col4Filler;
+    }
+
+    public void setCol4Filler(String col4Filler) {
+        this.col4Filler = col4Filler;
+    }
+
+    public String getPurposeCode() {
+        return purposeCode;
+    }
+
+    public void setPurposeCode(String purposeCode) {
+        this.purposeCode = purposeCode;
+    }
+
+    public String getCol7Filler() {
+        return col7Filler;
+    }
+
+    public void setCol7Filler(String col7Filler) {
+        this.col7Filler = col7Filler;
+    }
+
+    public String getCol8Filler() {
+        return col8Filler;
+    }
+
+    public void setCol8Filler(String col8Filler) {
+        this.col8Filler = col8Filler;
+    }
+
+    public String getPayerVpa() {
+        return payerVpa;
+    }
+
+    public void setPayerVpa(String payerVpa) {
+        this.payerVpa = payerVpa;
+    }
+
+    public String getCol9Filler() {
+        return col9Filler;
+    }
+
+    public void setCol9Filler(String col9Filler) {
+        this.col9Filler = col9Filler;
+    }
+
+    public String getCol10Filler() {
+        return col10Filler;
+    }
+
+    public void setCol10Filler(String col10Filler) {
+        this.col10Filler = col10Filler;
+    }
+
+    public String getPayeeVpa() {
+        return payeeVpa;
+    }
+
+    public void setPayeeVpa(String payeeVpa) {
+        this.payeeVpa = payeeVpa;
+    }
+
+    public String getParticipantId() {
+        return participantId;
+    }
+
+    public void setParticipantId(String participantId) {
+        this.participantId = participantId;
+    }
+
+    public String getRemIfscCode() {
+        return remIfscCode;
+    }
+
+    public void setRemIfscCode(String remIfscCode) {
+        this.remIfscCode = remIfscCode;
+    }
+
+    public String getCol11Filler() {
+        return col11Filler;
+    }
+
+    public void setCol11Filler(String col11Filler) {
+        this.col11Filler = col11Filler;
+    }
+
+    public String getRemAccountNo() {
+        return remAccountNo;
+    }
+
+    public void setRemAccountNo(String remAccountNo) {
+        this.remAccountNo = remAccountNo;
+    }
+
+    public String getCol12Filler() {
+        return col12Filler;
+    }
+
+    public void setCol12Filler(String col12Filler) {
+        this.col12Filler = col12Filler;
+    }
+
+    public String getCol13Filler() {
+        return col13Filler;
+    }
+
+    public void setCol13Filler(String col13Filler) {
+        this.col13Filler = col13Filler;
+    }
+
+    public String getCol14Filler() {
+        return col14Filler;
+    }
+
+    public void setCol14Filler(String col14Filler) {
+        this.col14Filler = col14Filler;
+    }
+
+    public String getCol15Filler() {
+        return col15Filler;
+    }
+
+    public void setCol15Filler(String col15Filler) {
+        this.col15Filler = col15Filler;
+    }
+
+    public String getInstitutionCode() {
+        return institutionCode;
+    }
+
+    public void setInstitutionCode(String institutionCode) {
+        this.institutionCode = institutionCode;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getTranAmtIntCur() {
+        return tranAmtIntCur;
+    }
+
+    public void setTranAmtIntCur(String tranAmtIntCur) {
+        this.tranAmtIntCur = tranAmtIntCur;
+    }
+
+    public String getMarkupRebate() {
+        return markupRebate;
+    }
+
+    public void setMarkupRebate(String markupRebate) {
+        this.markupRebate = markupRebate;
+    }
+
+    @Override
+    public String toString() {
+        return "Npci{" +
+                "txId='" + txId + '\'' +
+                ", transactionType='" + transactionType + '\'' +
+                ", col2Filler='" + col2Filler + '\'' +
+                ", tranSrlNo='" + tranSrlNo + '\'' +
+                ", responseCode='" + responseCode + '\'' +
+                ", tranDate='" + tranDate + '\'' +
+                ", tranTime='" + tranTime + '\'' +
+                ", tranAmount='" + tranAmount + '\'' +
+                ", col3Filler='" + col3Filler + '\'' +
+                ", col4Filler='" + col4Filler + '\'' +
+                ", purposeCode='" + purposeCode + '\'' +
+                ", col7Filler='" + col7Filler + '\'' +
+                ", col8Filler='" + col8Filler + '\'' +
+                ", payerVpa='" + payerVpa + '\'' +
+                ", col9Filler='" + col9Filler + '\'' +
+                ", col10Filler='" + col10Filler + '\'' +
+                ", payeeVpa='" + payeeVpa + '\'' +
+                ", participantId='" + participantId + '\'' +
+                ", remIfscCode='" + remIfscCode + '\'' +
+                ", col11Filler='" + col11Filler + '\'' +
+                ", remAccountNo='" + remAccountNo + '\'' +
+                ", col12Filler='" + col12Filler + '\'' +
+                ", col13Filler='" + col13Filler + '\'' +
+                ", col14Filler='" + col14Filler + '\'' +
+                ", col15Filler='" + col15Filler + '\'' +
+                ", institutionCode='" + institutionCode + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", currencyCode='" + currencyCode + '\'' +
+                ", tranAmtIntCur='" + tranAmtIntCur + '\'' +
+                ", markupRebate='" + markupRebate + '\'' +
+                '}';
+    }
+}
